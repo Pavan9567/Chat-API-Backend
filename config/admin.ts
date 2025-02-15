8a12a4596f47a43d3c81daa42b43209938f8e9d6
Pavan9567 <pavank9567@gmail.com>
2025-02-15 12:40:53 +0530

    Chat-Api Backend

--- config/admin.ts
+++ config/admin.ts
@@ -3,7 +3,7 @@ export default ({ env }) => ({
     secret: env('ADMIN_JWT_SECRET'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT','a2d3AQWhlHKvGZsh4XCNbw=='),
   },
   transfer: {
     token: {
